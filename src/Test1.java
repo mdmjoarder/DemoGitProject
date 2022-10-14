2a94d7cb8b18d67088161fbdf10126d622ab0864
Mahmudur Joarder <85805152+mahmudur7644@users.noreply.github.com>
2022-10-14 11:48:27 -0400

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,21 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String hungry ="Yes";
+		if(hungry == "yes"){
+			System.out.println("Eat something .....");
+		}else 
+		{
+			System.out.println("Do work .....");
+		}
+		
+		int a = 10;
+		int b = 20;
+		
+	int	sum = a+b;
+		System.out.println(sum);
+
+	}
+
+}
